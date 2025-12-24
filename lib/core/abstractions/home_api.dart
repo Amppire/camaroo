8f0c33718bd9472fbec934320c98772791771016
ShaunakV2 <shaunakvaghasia@gmail.com>
2025-12-24 16:36:22 -0500

    Removed test home widget code.

--- lib/core/abstractions/home_api.dart
+++
@@ -1,6 +0,0 @@
-abstract class HomeApi {
-  int get counter;
-  void setCounter(int value);
-  Function(int) onCounterChanged = (value) {};
-  void setOnCounterChanged(Function(int) callback) => onCounterChanged = callback;
-}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:camaroo/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const Cameroo());
+    await tester.pumpWidget(Cameroo());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
